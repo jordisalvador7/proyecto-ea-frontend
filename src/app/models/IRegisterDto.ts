0b388293ecb8ad8e2b71c437fefdb7088d467c5a
Arnau <arnau27@pm.me>
2020-04-02 19:34:57 +0200

    Register Working

---
+++ src/app/models/IRegisterDto.ts
@@ -0,0 +1,5 @@
+export interface IRegisterDto {
+    Username: string;
+    Email: string;
+    Password: string
+}
--- src/app/registration/registration.page.ts
+++ src/app/registration/registration.page.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { StorageService } from '../services/storage.service';
 import { Router } from '@angular/router';
+import { IRegisterDto } from '../models/IRegisterDto';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Component({
@@ -11,42 +13,59 @@ import { Observable } from 'rxjs';
 })
 export class RegistrationPage implements OnInit {
 
-  postData = {
+  // http://localhost:3700
+  url: string = 'http://localhost:3700/auth/register';
+  headers = new HttpHeaders({'Content-Type':'application/json'});
+
+  registerDto: IRegisterDto = {
     Username: '',
     Email: '',
     Password: ''
-  };
+  }
+
+  // body = { Username: "username85", Email: 'email@email89.com', Password: '12345678'}
+  registerReq = (dto: IRegisterDto) => {
+    console.log(`Sending: ${dto}`);
+    this.http.post<IRegisterDto>(this.url, dto)
+      .subscribe( res => { console.log(res) });
+  }
+
+  getHome = () => {
+
+  }
 
   constructor(
+    private http: HttpClient,
     private authService: AuthService,
     private storageService: StorageService,
     private router: Router) { }
-
-  ngOnInit() {}
-
-  validateInputs() {
-    console.log(this.postData);
-    let username = this.postData.Username.trim();
-    let password = this.postData.Password.trim();
-    let email = this.postData.Email.trim();
-    return (
-    this.postData.Username &&
-    this.postData.Password &&
-    this.postData.Email &&
-    username.length > 0 &&
-    email.length > 0 &&
-    password.length > 0
-    );
-  }
+    
+    ngOnInit() {}
+    
+    // validateInputs() {
+  //   console.log(this.registerData);
+  //   let username = this.registerData.Username.trim();
+  //   let password = this.registerData.Password.trim();
+  //   let email = this.registerData.Email.trim();
+  //   return (
+  //   this.registerData.Username &&
+  //   this.registerData.Password &&
+  //   this.registerData.Email &&
+  //   username.length > 0 &&
+  //   email.length > 0 &&
+  //   password.length > 0
+  //   );
+  // }
 
   registerAction(){
-    if(this.validateInputs()){
-      console.log(this.postData);
-    }
-    else{
-      console.log('Fill all the fields please');
-    }
-
+    console.log(this.registerDto);
+    this.registerReq(this.registerDto);
+    // if(this.validateInputs()){
+    //   console.log(this.registerData);
+    // }
+    // else{
+    //   console.log('Fill all the fields please');
+    // }
   }
 
   /*POST: add a new hero to the database 
