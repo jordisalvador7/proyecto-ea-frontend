bb3d309568a957be803e691794db8fa9086c749d
Arnau <arnau27@pm.me>
2020-04-14 22:20:30 +0200

    Generics added

--- src/app/pages/profile/profile.page.ts
+++ src/app/pages/profile/profile.page.ts
@@ -2,6 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { HttpService } from 'src/app/services/http/http.service';
 import { map } from 'rxjs/operators';
 
+// Objeto modesto
+type UserProfile = {     
+  id: string,
+  username: string,
+  email: string,
+  history: any[]
+}
 
 @Component({
   selector: 'app-profile',
@@ -9,22 +16,35 @@ import { map } from 'rxjs/operators';
   styleUrls: ['./profile.page.scss'],
 })
 export class ProfilePage implements OnInit {
-  private userProfile = {
+  
+  private userProfile: UserProfile = {
     id: '',
     username: '',
     email: '',
     history: []
   }
-
   constructor(private http: HttpService) { }
-
+  
   ngOnInit() {
-    this.http.get('/profile').pipe(map(profile => {
-      this.userProfile.id = profile['_id'];
-      this.userProfile.username = profile['Username'];
-      this.userProfile.email = profile['Email'];
-      this.userProfile.history = profile['History'];
-    })).subscribe();
-  }
 
-}
+    // this.http.get('/profile').pipe(map(profile => {
+    //     this.userProfile.id = profile['_id'];
+    //     this.userProfile.username = profile['Username'];
+    //     this.userProfile.email = profile['Email'];
+    //     this.userProfile.history = profile['History'];
+    //   })).subscribe();
+      
+      this.http.get('/profile').subscribe( (profile: Profile) => { 
+        this.userProfile.id = profile._id;
+        this.userProfile.username = profile.Username;
+        this.userProfile.email = profile.Email;
+        this.userProfile.history = profile.History;
+      });
+    }
+  }
+  interface Profile {
+    _id: string,
+    Username: string,
+    Email: string,
+    History: any[] //tipar
+  }
--- src/app/services/http/http.service.ts
+++ src/app/services/http/http.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { StorageService } from '../storage/storage.service';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -24,8 +25,8 @@ export class HttpService {
     })
   }
 
-  get = (endpoint: string) =>
-    this.http.get(`${this.URL}${endpoint}`, { headers: this.headers });
+  get = <TResponse>(endpoint: string): Observable<TResponse> =>
+    this.http.get<TResponse>(`${this.URL}${endpoint}`, { headers: this.headers });
 
   // get = (endpoint: string) => {
   //   this.http.get(`${this.URL}${endpoint}`, { headers: this.headers })
