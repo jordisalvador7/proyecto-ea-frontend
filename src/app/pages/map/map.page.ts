7d980f836ccc8617729aac72d6b6e7e0350b0e28
jordisalvador7 <jordisalvador7@users.noreply.github.com>
2020-05-06 19:07:42 +0200

    added map view

--- src/app/pages/map/map.page.ts
+++ src/app/pages/map/map.page.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import L from "leaflet";
+import 'leaflet-routing-machine';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.page.html',
   styleUrls: ['./map.page.scss'],
 })
-export class MapPage implements OnInit {
+export class MapPage {
 
-  constructor() { }
+  map: L.Map;
+  center: L.PointTuple;
+  startCoords = [41.276048, 1.986713];
 
-  ngOnInit() {
+  constructor(public platform: Platform) 
+  {
+    this.center = this.startCoords;
+    this.platform.ready().then(() =>
+    {
+      this.leafletMap();
+    })
   }
 
+  leafletMap()
+  {
+    this.map = L.map('mapId', 
+    {
+      center: this.center,
+      zoom: 15
+    });
+
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+      attribution: '', 
+    }).addTo(this.map);
+
+   
+  }
+ 
 }
