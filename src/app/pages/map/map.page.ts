1b84684172718b356bbf42c0f9917a1aa29c2800
jordisalvador7 <jordisalvador7@users.noreply.github.com>
2020-05-11 00:50:13 +0200

    Get location added

--- src/app/pages/map/map.page.ts
+++ src/app/pages/map/map.page.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
-import L from 'leaflet';
+import { Map, latLng, tileLayer, Layer, marker } from 'leaflet';
 import 'leaflet-routing-machine';
 import { Platform } from '@ionic/angular';
-import { Geolocation } from '@ionic-native/geolocation/ngx';
+//import { Geolocation } from '@ionic-native/geolocation/ngx';
+import { Plugins } from '@capacitor/core';
+
+
+const { Geolocation } = Plugins;
 
 @Component({
   selector: 'app-map',
@@ -11,40 +15,27 @@ import { Geolocation } from '@ionic-native/geolocation/ngx';
 })
 export class MapPage {
 
-  lat:any=''
-  lng:any=''
+  latitude: number;
+  longitude: number;
 
-  map: L.Map;
-  center: L.PointTuple;
-  startCoords = [this.lat, this.lng];
+  map: Map;
+  //center: PointTuple;
+  startCoords = [this.latitude, this.longitude];
 
-  constructor(
-    public platform: Platform,
-    private geolocation: Geolocation) 
-  {
-    this.center = this.startCoords;
-    this.platform.ready().then(() =>
-    {
-      this.leafletMap();
-    })
-  }
+  constructor()  {  }
+
+  ionViewDidEnter() { this.leafletMap(); }
 
   leafletMap()
   {
-    this.map = L.map('mapId', 
-    {
-      center: this.center,
-      zoom: 15
-    });
-
-    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-      attribution: '', 
+    this.map = new Map('mapId').setView([41.27555556, 1.98694444], 15);
+    tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
+      attribution: 'edupala.com © ionic LeafLet',
     }).addTo(this.map);
 
-   
   }
 
-  async getLocation(){
+  /*async getLocation(){
     this.geolocation.getCurrentPosition(
       {maximumAge: 1000, timeout: 5000,
        enableHighAccuracy: true }
@@ -63,6 +54,20 @@ export class MapPage {
             //alert('Error getting location'+JSON.stringify(error));
             alert('Error getting location - '+JSON.stringify(error))
             });
+  }*/
+
+  async getCurrentPosition() {
+    const position = await Geolocation.getCurrentPosition();
+    this.latitude = position.coords.latitude;
+    this.longitude = position.coords.longitude;
+    console.log('Current', position);
+    this.map.setView([this.latitude, this.longitude], 15);
+    tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
+      attribution: 'edupala.com © ionic LeafLet',
+    }).addTo(this.map);
+    marker([this.latitude, this.longitude]).addTo(this.map)
+      .bindPopup('You are here')
+      .openPopup();
   }
  
 }
