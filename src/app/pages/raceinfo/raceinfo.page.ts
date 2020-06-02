67bbec1a7e90def8d31b46bb8b15186182a679dd
jordisalvador7 <jordisalvador7@users.noreply.github.com>
2020-06-03 00:50:06 +0200

    implemented map in raceinfo

--- src/app/pages/raceinfo/raceinfo.page.ts
+++ src/app/pages/raceinfo/raceinfo.page.ts
@@ -3,13 +3,14 @@ import { Racemodel } from './../../models/race/racemodel';
 import { HttpService } from './../../services/http/http.service';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Map, latLng, tileLayer, Layer, marker } from 'leaflet';
 
 @Component({
   selector: 'app-raceinfo',
   templateUrl: './raceinfo.page.html',
   styleUrls: ['./raceinfo.page.scss'],
 })
-export class RaceinfoPage implements OnInit {
+export class RaceinfoPage {
 
   data:any;
 
@@ -27,26 +28,32 @@ export class RaceinfoPage implements OnInit {
    }
 
   races: Racemodel[];
+  me: Usermodel;
+  map: Map;
+  latitude: number;
+  longitude: number;
 
-  ngOnInit() {
-    /*this.http.get<Racemodel[]>('/races/races').subscribe(
-      (races:Racemodel[]) => {
-        this.races= races;
-        console.log(this.races);
-        this.loadSubs();
-      }
-    );*/
+  async ionViewDidEnter() {
+    await this.http.setOptionsAsync();
+    this.leafletMap();
+    marker([this.latitude, this.longitude]).addTo(this.map)
+      .bindPopup('<b> Starting Point </b>')
+      .openPopup();
   }
 
-  /*loadSubs(){
-    this.races.forEach(race => {
-      this.http.get<Usermodel[]>('/races/getsubs/' + race._id).subscribe(
-        (subs:Usermodel[]) => {
-          race.subscribers = subs;
-        }
-      )
-    });
+  async leafletMap()
+  {
+    this.map = new Map('mapId');
+    this.latitude = this.data.startingPoint.coordinates[1];
+    this.longitude = this.data.startingPoint.coordinates[0];
+    console.log(this.latitude , this.longitude);
+    this.map.setView([this.latitude, this.longitude], 15);
+    tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
+      attribution: 'edupala.com © ionic LeafLet',
+    }).addTo(this.map);
   }
+
+  
 
   async Subscribe(race: Racemodel){
     console.log("subscribe to");
@@ -61,6 +68,16 @@ export class RaceinfoPage implements OnInit {
     await this.http.post<any>('/races/unsubscribe/' +  race._id).toPromise();
     //window.location.reload();
     //this.getNearPlaces();
-  }*/
+  }
+
+  AmISubscribed(race: Racemodel):Boolean{
+    let veredict:boolean = false;
+    race.subscribers.forEach(sub => {
+      if(sub._id === this.me._id || sub === this.me._id){
+        veredict = true;
+      }
+    });
+    return veredict;
+  }
 
 }
