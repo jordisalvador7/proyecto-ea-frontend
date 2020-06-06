eeaf9691b405d8c721af16f600e55100d06a1a64
AntonEstrela <anton.estrela@gmail.com>
2020-06-07 00:39:57 +0200

    funciona tot

--- src/app/models/IProfile.ts
+++ src/app/models/IProfile.ts
@@ -1,6 +1,9 @@
+import { Racemodel } from './race/racemodel';
+
 export interface IProfile {
     _id: string,
     username: string,
     email: string,
-    history: any[] //tipar
+    history: Racemodel[],
+    pending?: Racemodel[]
   }
