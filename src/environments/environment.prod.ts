f238e7151019c09f8ac1de82a0adb75ab1cf0f93
AntonEstrela <anton.estrela@gmail.com>
2020-06-28 16:14:31 +0200

    .

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCaQ1u74cFmQQrPpfVME02Fx3dTLePd-Ek",
+    authDomain: "runnea.firebaseapp.com",
+    databaseURL: "https://runnea.firebaseio.com",
+    projectId: "runnea",
+    storageBucket: "runnea.appspot.com",
+    messagingSenderId: "590722008925",
+    appId: "1:590722008925:web:ea8f783c047e0876cd4ab4"
+  }
 };
